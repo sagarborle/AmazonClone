1ebbfded969d5bbbc226c842a96914d8baf60eb2
Sagar Borle <47034112+sagarborle@users.noreply.github.com>
2020-09-21 18:42:46 +0530

    cartcheckout in process

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import "./Checkout.css";
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
+    const [{basket},dispatch]=useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <img className="checkout_ad"
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                alt=""/>
+                alt=""
+                />
         
                 <div >
                     <h2 className="checkout_title">Your Shopping Cart</h2>
+                    
+                     {/* <CheckoutProduct
+                        id='4531315'
+                        title='This is the test, lorem ipsum lorem ipsumlorem ipsum
+                        lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._ACSY400_.jpg"
+                        price={265.33}
+                        rating={4}
+                        /> */}
 
-                    {/* BasketItem */}
-                    {/* BasketItem */}
-                    {/* BasketItem */}
-                    {/* BasketItem */}                
+                    
+                    {basket.map(item => (
+                        <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                       />
+
+                    ))}
+                    {/* CheckoutProduct */}
+                                   
+                    {/* CheckoutProduct */}
+                                   
+                    {/* CheckoutProduct */}
+                                   
+
+
+                            
                 </div>
             
             </div>
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,10 @@ export const initialState={
 
 };
 
+//Selector
+export const getBasketTotal=(basket)=>
+basket?.reduce((amount, item) => item.price + amount,0);
+
 const reducer=(state,action)=>{
     console.log(action);
     switch(action.type){
